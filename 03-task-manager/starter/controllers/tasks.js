34c8c72828e29585360cf205b1ef6ad8d89bad3e
Nino Chimento <nchimento@byte-code.com>
2021-10-22 11:14:54 +0200

    task manager crud complete

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -40,7 +40,7 @@ const updateTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task id ${taskId}` });
     }
-    res.status(200).json({ id: taskId, data: req.body });
+    res.status(200).json({ id: taskId, data: req.body, oldData: task });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
